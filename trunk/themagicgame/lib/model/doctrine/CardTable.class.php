4c92e9f93daf8810abd984d6d87a2ff2cbcc4c0f
martincammi@gmail.com <martincammi@gmail.com@2ac14240-5f31-0eba-9759-45d317262017>
2010-08-11 04:49:39 +0000

    UI Design

--- trunk/themagicgame/lib/model/doctrine/CardTable.class.php
+++ trunk/themagicgame/lib/model/doctrine/CardTable.class.php
@@ -8,4 +8,14 @@ class CardTable extends Doctrine_Table
     {
         return Doctrine_Core::getTable('Card');
     }
+    
+	public static function loadCard($cardId){
+	  	
+	    $card = Doctrine::getTable('Card')->find(array($cardId));
+	    $expansion = Doctrine::getTable('Expansion')->find(array($card->getIdexpansion()));
+	    
+	    $card->setIdexpansion($expansion); 
+	    
+	    return $card;  
+	}
 }
