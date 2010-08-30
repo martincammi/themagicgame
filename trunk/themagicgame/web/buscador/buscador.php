eddd04831731844b24a6f9d6165e46d9205c2993
martincammi@gmail.com <martincammi@gmail.com@2ac14240-5f31-0eba-9759-45d317262017>
2010-08-30 03:57:21 +0000

    Lot of changes, UI css, and functionality of the search bar, now is working

--- trunk/themagicgame/web/buscador/buscador.php
+++ trunk/themagicgame/web/buscador/buscador.php
@@ -4,6 +4,9 @@
 
 <!-- Todo lo que diga ACCION:algo hay que reemplazarlo por la acciòn correspondiente -->
 
+<script type="text/javascript" src="/js/functions.js"></script>
+
+
 <script>
 
 function changeTab(link){
@@ -14,8 +17,12 @@ function changeTab(link){
 	$('.search-btn-option').show();
 }
 
-function submitMe(){
-	$('search-box').submit();
+function submitSearch(){
+	var formId= 'search-box-form';
+	var searchText = document.getElementById('searchBox').value;
+	var form = document.getElementById(formId);
+	form.elements["searchBox"].value = searchText;
+	submitById(formId);
 }
 
 </script>
@@ -24,21 +31,23 @@ function submitMe(){
 		<div class="taringa-search">
 			<div class="taringa-bar">
 				<ul class="search-options"  style="height:27px;	margin:0 auto;	padding:0;	width:610px;">
-					<li class="active"><a href="/frontend_dev.php/" onclick="changeTab( this);">Home</a></li>
+					<li class="active"><a href="<?php echo url_for('/') ?>" onclick="changeTab( this);">Home</a></li>
 					<li><a href="" onclick="changeTab(this); return false">Listados</a></li>
 					<li><a href="" onclick="changeTab(this); return false">Sets</a></li>
 					<li id="logo"></li>
 				</ul>
 			</div>
 
-			<div class="search clearfix">
+			<div class="search">
 				<div class="search-box">
-					<form class="clearfix" id="search-box" style="padding:0;margin:0" action="<?php echo url_for('/card/index') ?>" method="get" onsubmit="window.search.onsubmit()">
-						<div class="input-left"></div>			
-						<input type="text" class="sinput" id="searchBox" >
+					<form class="clearfix" id="search-box-form" style="padding:0;margin:0" action="<?php echo url_for('/card/index') ?>" method="get">
+						<div class="input-left"></div>
+						<div id="pepe">
+							<input id="searchBox" name="searchBox" type="text" class="search-input" value="vamp"/>
+						</div>
 						<div class="input-right"></div>
 						<div class="btn-search floatL">
-							<a href="algo"></a>
+							<a id="search-link" onclick="submitSearch();"></a>
 						</div>
 					</form>
 					
@@ -56,6 +65,7 @@ function submitMe(){
 		</div>
 	</div>
 </div>
+
 <script type="text/javascript">
-	$('searchBox').focus();
+	//$('searchBox').focus();
 </script>
