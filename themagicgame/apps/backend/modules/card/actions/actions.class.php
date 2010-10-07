d7757a474db083ed54541dcc87f15339bedcba92
martincammi@gmail.com <martincammi@gmail.com@2ac14240-5f31-0eba-9759-45d317262017>
2010-10-07 00:02:05 +0000

    Adding teh Backend

---
+++ themagicgame/apps/backend/modules/card/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/cardGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/cardGeneratorHelper.class.php';
+
+/**
+ * card actions.
+ *
+ * @package    themagicgame
+ * @subpackage card
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class cardActions extends autoCardActions
+{
+}
---
+++ themagicgame/cache/backend/dev/modules/autoCard/templates/_list_th_stacked.php
@@ -0,0 +1,1 @@
+<?php include_partial('card/list_th_tabular', array('sort' => $sort)) ?>
